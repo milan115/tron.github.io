afd169a6c64076af92571d056ee401be2062662f
milan115 <milanlm798@gmail.com>
2021-10-19 19:05:44 +0600

    tron

---
+++ precache-manifest.30af6e0b0b9f551a227ff8920c3c50bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b226033b52e0c490e9",
+    "url": "/tron/static/css/main.64e803de.chunk.css"
+  },
+  {
+    "revision": "a3b226033b52e0c490e9",
+    "url": "/tron/static/js/main.9efebfd3.chunk.js"
+  },
+  {
+    "revision": "10b0d392e5dd3608b72f",
+    "url": "/tron/static/js/runtime~main.c30d5cd5.js"
+  },
+  {
+    "revision": "207ba01926414a7e24f3",
+    "url": "/tron/static/js/2.c0004ce2.chunk.js"
+  },
+  {
+    "revision": "0fcc8cbe2704aef05d5c4d311b4dab89",
+    "url": "/tron/index.html"
+  }
+];
